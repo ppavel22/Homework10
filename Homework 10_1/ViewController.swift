929c5cbb4151f6562339a5fdaad49fd487f6b9b7
Pavel Chekalkin <chekalkin.pavel@gmail.com>
2017-11-05 20:58:28 +0200

    First commit

---
+++ Homework 10_1/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Homework 10_1
+//
+//  Created by mac on 05.11.2017.
+//  Copyright © 2017 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    @IBAction func firstBackPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func secondBackPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
